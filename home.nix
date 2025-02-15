a98ea7583ac2f11ec76adb3eb751043126b536b7
TheComet <alex.murray@gmx.ch>
2025-02-15 16:43:38 +0100

    First lsp.lua

--- home.nix
+++ home.nix
@@ -1,9 +1,19 @@
 { config, pkgs, pkgs-unstable, ... }:
 {
-  xdg.configFile."nvim" = {
-    source = ./nvim-config;
-    recursive = true;
-  };
+  xdg.configFile."nvim/init.lua".text = builtins.readFile ./nvim-config/init.lua;
+  xdg.configFile."nvim/lua/thecomet/options.lua".text = builtins.readFile ./nvim-config/lua/thecomet/options.lua;
+  xdg.configFile."nvim/lua/thecomet/keymap.lua".text = builtins.readFile ./nvim-config/lua/thecomet/keymap.lua;
+
+  xdg.configFile."nvim/lua/thecomet/justifier.py".source = ./nvim-config/lua/thecomet/justifier.py;
+
+  xdg.configFile."nvim/lua/thecomet/colorscheme.lua".text = builtins.readFile ./nvim-config/lua/thecomet/colorscheme.lua;
+  xdg.configFile."nvim/lua/thecomet/completion.lua".text = builtins.readFile ./nvim-config/lua/thecomet/completion.lua;
+  xdg.configFile."nvim/lua/thecomet/context.lua".text = builtins.readFile ./nvim-config/lua/thecomet/context.lua;
+  xdg.configFile."nvim/lua/thecomet/lsp.lua".text = builtins.readFile ./nvim-config/lua/thecomet/lsp.lua;
+  xdg.configFile."nvim/lua/thecomet/mini.lua".text = builtins.readFile ./nvim-config/lua/thecomet/mini.lua;
+  xdg.configFile."nvim/lua/thecomet/multicursor.lua".text = builtins.readFile ./nvim-config/lua/thecomet/multicursor.lua;
+  xdg.configFile."nvim/lua/thecomet/telescope.lua".text = builtins.readFile ./nvim-config/lua/thecomet/telescope.lua;
+  xdg.configFile."nvim/lua/thecomet/treesitter.lua".text = builtins.readFile ./nvim-config/lua/thecomet/treesitter.lua;
 
   home = {
     username = "thecomet";
@@ -30,10 +40,12 @@
     neovim = {
       enable = true;
       extraPackages = with pkgs; [
-        gcc     # Treesitter
-        unzip   # Mason needs to unzip clangd
-        ripgrep # Telescope
-        python3 # justifier.py
+        gcc          # Treesitter
+        unzip        # Mason needs to unzip clangd
+        ripgrep      # Telescope
+        python3      # justifier.py
+        clang-tools  # C/C++ language server for LSP client
+        lua-language-server
       ];
       plugins = with pkgs.vimPlugins; [
         # Colorschemes
@@ -43,33 +55,25 @@
         nightfox-nvim
         nightfly
 
-        # C/C++
-        cmake-tools-nvim
-        trouble-nvim
-
-        # Git
-        conflict-marker-vim
-        fugitive
-
         # Misc
-        harpoon
+        #harpoon
         mini-cursorword
         mini-hipatterns
         mini-splitjoin
         mini-surround
         multicursors-nvim
         rainbow-delimiters-nvim
-        undotree
-        vim-tmux-navigator
-        which-key-nvim
+        #undotree
+        #vim-tmux-navigator
+        #which-key-nvim
 
-        # Completion
-        luasnip
-        nvim-cmp
-        cmp-nvim-lua
-        cmp-nvim-lsp
-        cmp-buffer
-        cmp_luasnip
+        # Telescope
+        telescope-nvim
+        telescope-undo-nvim
+
+        # Git
+        #conflict-marker-vim
+        #fugitive
 
         # Treesitter
         (nvim-treesitter.withPlugins (p: with p; [
@@ -78,12 +82,23 @@
           nix passwd python regex rust sql ssh_config toml udev vim vimdoc xml
           yaml
         ]))
-        # Enable/Disable toggle not fixed in 24.11
-        pkgs-unstable.vimPlugins.nvim-treesitter-context
+        pkgs-unstable.vimPlugins.nvim-treesitter-context  # Enable/Disable toggle not fixed in 24.11
 
-        # Telescope
-        telescope-nvim
-        telescope-undo-nvim
+        # LSP
+        lazydev-nvim    # Lua completion for nvim
+        nvim-lspconfig
+
+        # Completion
+        #luasnip
+        #nvim-cmp
+        #cmp-nvim-lua
+        #cmp-nvim-lsp
+        #cmp-buffer
+        #cmp_luasnip
+
+        # C/C++
+        cmake-tools-nvim
+        #trouble-nvim
 
         # TODO:
         #   FabijanZulj/blame.nvim
